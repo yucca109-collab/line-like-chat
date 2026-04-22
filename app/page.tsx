3c95d397f2b327ecfb96d211ae72659869cad4dc
yucca109-collab <yucca109@gmail.com>
2026-04-22 10:45:39 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1 style={{ fontSize: 32 }}>案件管理システム</h1>
-      <p>ここにLINE風チャットを作ります。</p>
-    </main>
-  );
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/login");
 }
