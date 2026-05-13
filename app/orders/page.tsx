8cb54d6a730dec38486b5ad4a1c3a3a9bde6699f
yucca109-collab <yucca109@gmail.com>
2026-05-14 01:23:51 +0900

    Update page.tsx

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useMemo, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter, useSearchParams } from "next/navigation";
